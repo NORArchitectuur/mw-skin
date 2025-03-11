838193c96d2e1ee5da36926af710f5065d7084fd
Rogier Mechielsen <mail@rogiermechielsen.nl>
2025-03-11 13:12:49 +0100

    add styling for dropdowns

---
+++ nds-dev/demo.js
@@ -0,0 +1,7 @@
+import '../resources/js/export-rdf';
+import '../resources/js/icon-only';
+import '../resources/js/information-panel';
+import '../resources/js/navigation';
+import '../resources/js/smartcomments';
+import '../resources/js/submenus';
+import '../resources/js/toc';
