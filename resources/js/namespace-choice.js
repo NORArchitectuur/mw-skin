feb98fd53d8137fdadcdf65644b26f549f5edf11
Youri van den Bogert <yvdbogert@archixl.nl>
2025-03-12 09:45:08 +0100

    Refactor namespace choice and icon portlet JavaScript

--- resources/js/namespace-choice.js
+++ resources/js/namespace-choice.js
@@ -1,32 +1,17 @@
 /**
  * JavaScript for handling namespace choice radio buttons
  */
-( function () {
+$( document ).ready( function () {
 	'use strict';
 
-	/**
-	 * Add event listeners to namespace choice radio buttons
-	 */
-	function setupNamespaceChoiceHandlers() {
-		const radioButtons = document.querySelectorAll('.namespace-choice-radio');
-		
-		radioButtons.forEach(function(radio) {
-			radio.addEventListener('change', function() {
-				if (this.checked) {
-					// Navigate to the URL stored in the data-href attribute
-					const href = this.getAttribute('data-href');
-					if (href) {
-						window.location.href = href;
-					}
-				}
-			});
-		});
-	}
-
-	// When the document is ready
-	if (document.readyState === 'loading') {
-		document.addEventListener('DOMContentLoaded', setupNamespaceChoiceHandlers);
-	} else {
-		setupNamespaceChoiceHandlers();
-	}
-}() ); 
+	// Add event listeners to namespace choice radio buttons
+	$( document ).on( 'change', '.namespace-choice-radio', function() {
+		if ( $( this ).prop( 'checked' ) ) {
+			// Navigate to the URL stored in the data-href attribute
+			const href = $( this ).attr( 'data-href' );
+			if ( href ) {
+				window.location.href = href;
+			}
+		}
+	});
+} );
