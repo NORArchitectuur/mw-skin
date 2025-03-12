4141db7eea31b6ab497e1417bdc43b4db6c837ca
Rogier Mechielsen <mail@rogiermechielsen.nl>
2025-03-12 10:37:50 +0100

    update html and js

--- nds-dev/demo.js
+++ nds-dev/demo.js
@@ -1,6 +1,7 @@
 import '../resources/js/export-rdf';
 import '../resources/js/icon-only';
 import '../resources/js/information-panel';
+import '../resources/js/namespace-choice';
 import '../resources/js/navigation';
 import '../resources/js/smartcomments';
 import '../resources/js/submenus';
